ce3a34d18c350a6fb7d5f9818613a8b02c9076b2
Resty John Yuson <tyres9@yahoo.com>
2024-02-05 21:58:07 +0900

    Add Profile features

--- Blog_project_django/users/views.py
+++ Blog_project_django/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == "POST":
@@ -9,8 +11,20 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account is created for {username}")
-            return redirect("blog-home")
+            messages.success(request, f"Your account has been created. You are able to log in ")
+            return redirect("login")
     else:    
         form = UserRegistrationForm()
     return render(request, 'users/register.html',{'form': form})
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return render(request,'users/logout.html')
+
+@login_required
+def profile(request):
+    return render(request,'users/profile.html')
+
+    
+        
